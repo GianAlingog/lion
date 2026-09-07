c32466b4f84b02ef2a843fc1f4f56abe601e78ce
Gian Alingog <gianqalingog@gmail.com>
2026-09-07 19:21:57 +0800

    Blueprint Garbage

---
+++ crates/engine/src/garbage.rs
@@ -0,0 +1,28 @@
+pub struct GarbageBatch {
+    pub lines: u8,
+    pub hole: u8,
+    pub delay: u16,
+}
+
+pub struct GarbageQueue {
+    // Vec over VecDeque since this should remain small in size
+    pending: Vec<GarbageBatch>,
+}
+
+impl GarbageQueue {
+    pub fn incoming(&self) -> u32 {
+        todo!("Return number of incoming lines in the queue")
+    }
+
+    pub fn push(&mut self, batch: GarbageBatch) {
+        todo!("Push the garbage to the vec; handle other initializations in the future")
+    }
+
+    pub fn cancel(&mut self, amount: u32) -> u32 {
+        todo!("Should return number of attach left, use in while loop")
+    }
+
+    pub fn tick(&mut self) -> Vec<GarbageBatch> {
+        todo!("Return the batches that are to be pushed")
+    }
+}
