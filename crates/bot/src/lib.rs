fdf5fbd5f6b4e0b43f036985d9862f474794f896
Gian Alingog <gianqalingog@gmail.com>
2026-09-10 12:10:13 +0800

    Blueprint Bot

--- crates/bot/src/lib.rs
+++ crates/bot/src/lib.rs
@@ -1,1 +1,17 @@
+use engine::{game::Game, piece::Placement, srs::SpinKind};
 
+pub struct Move {
+    pub placement: Placement,
+    pub spin: SpinKind,
+    pub use_hold: bool,
+}
+
+pub trait Bot {
+    fn pick(&mut self, _game: Game) -> Option<Move> {
+        todo!()
+    }
+
+    fn name(&self) -> &str {
+        todo!()
+    }
+}
