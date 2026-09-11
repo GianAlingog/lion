2b790274d353117d662a4e3af7f745f5805ed46f
Gian Alingog <gianqalingog@gmail.com>
2026-09-11 22:21:16 +0800

    Blueprint command line run

--- crates/arena/src/main.rs
+++ crates/arena/src/main.rs
@@ -1,6 +1,21 @@
 pub mod run;
 pub mod stats;
 
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+struct Args {
+    games: u32,
+    seed: u64,
+    mode: String,
+    max_pieces: u32,
+    csv: PathBuf,
+    // bot,
+    // weights,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{args:?}");
 }
